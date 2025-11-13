f03011649187ee5ac586544c7a9869946746a6b0
nktkas <github.turk9@passmail.net>
2025-11-13 18:45:18 +0200

    test(subscription): relaxation of coverage for `userHistoricalOrders`

--- tests/api/subscription/userHistoricalOrders.test.ts
+++ tests/api/subscription/userHistoricalOrders.test.ts
@@ -18,7 +18,7 @@ runTest({
       ignoreEmptyArray: ["#/properties/orderHistory/items/properties/order/properties/children"],
       ignoreBranches: {
         "#/properties/orderHistory/items/properties/order/properties/orderType": [2, 3, 4, 5],
-        "#/properties/orderHistory/items/properties/order/properties/tif/wrapped": [2, 4],
+        "#/properties/orderHistory/items/properties/order/properties/tif/wrapped": [1, 2, 4],
         "#/properties/orderHistory/items/properties/status": [
           3,
           4,
