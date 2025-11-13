cbc5b5398d55b700d8b3fd0cf2b38039793768d6
nktkas <github.turk9@passmail.net>
2025-11-13 17:43:50 +0200

    test(info): update testing address for `userDetails` test

--- tests/api/info/userDetails.test.ts
+++ tests/api/info/userDetails.test.ts
@@ -6,12 +6,12 @@ runTest({
   name: "userDetails",
   codeTestFn: async (_t, client) => {
     const data = await Promise.all([
-      client.userDetails({ user: "0xe019d6167E7e324aEd003d94098496b6d986aB05" }),
+      client.userDetails({ user: "0x9150749C4cec13Dc7c1555D0d664F08d4d81Be83" }),
     ]);
     schemaCoverage(UserDetailsResponse, data);
   },
   cliTestFn: async (_t, runCommand) => {
-    const data = await runCommand(["info", "userDetails", "--user", "0xe019d6167E7e324aEd003d94098496b6d986aB05"]);
+    const data = await runCommand(["info", "userDetails", "--user", "0x9150749C4cec13Dc7c1555D0d664F08d4d81Be83"]);
     parser(UserDetailsRequest)(JSON.parse(data));
   },
 });
